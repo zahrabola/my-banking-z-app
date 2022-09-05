f4d75c5830a86b37e565e1ccd2195c8bc93cb571
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-09-05 20:08:17 +0100

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 
 import './App.css';
+import CardDeal from "./components/CardDeal";
+import Business from './components/Business';
+import Billing from './components/Billing';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Stats from './components/Stats';
+import Currency from './components/Currency';
+import Stocks from './components/Stocks';
+import Testimonials from './components/Testimonials';
+import Clients from './components/Clients';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-     <div>Zahra Olanrewaju </div> 
+      <div>Zahra Olanrewaju </div>
+      <div className="container ">
+        <div className="app-bg">
+          <div className="box">
+            <Navbar />
+          </div>
+        </div>
+        <div className="app-bg">
+          <div className="box">
+            <Hero />
+          </div>
+        </div>
+        <div className="app-bg">
+          <div className="box">
+            <Stats />
+            <Business />
+            <Currency />
+            <Billing />
+            <CardDeal />
+            <Stocks />
+            <Testimonials />
+            <Clients />
+            <CTA /> 
+            <Footer /> 
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,48 @@
+import {useState} from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constatnts/Data";
+
+
 const Navbar = () => {
-  return <div> Navbar</div>;
+  const [toggle, setToggle] = useState(false);
+
+
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-logo">
+          <img src={logo} alt="z-bank" className="z-bank" />
+        </div>
+        <div className="nav-container">
+          <ul className="nav-menu">
+            {navLinks.map((nav, index) => (
+              <li key={nav.id} className="nav-item ">
+                <a>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="nav-icon">
+            <img
+              src={toggle ? close : menu}
+              alt="menu-bank"
+              onClick={() => setToggle(!toggle)}
+            />
+          </div>
+        </div>
+      </nav>
+      <div>
+        <div className="nav-sb">
+          {navLinks.map((nav, index) => (
+            <div key={nav.id} className="nav-sb">
+              <a className="sb">{nav.title}</a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+
+        
 };
 export default Navbar;
