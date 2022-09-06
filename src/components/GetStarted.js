daabcdd6778f90ee7a6585d7b111f48774e8f18c
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-09-06 16:02:30 +0100

    Update hero

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,4 +1,22 @@
+import { arrowUp } from "../assets";
+
 const GetStarted = () => {
-  return <div> Get Started </div>;
+  return (
+    <div>
+      <div className="gs-circle">
+        <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+          <span className="text-gradient">Get</span>
+        </p>
+        <img
+          src={arrowUp}
+          alt="arrow-up"
+          className="w-[23px] h-[23px] object-contain"
+        />
+        <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 export default GetStarted;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,38 @@
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+
 const Hero = () => {
-  return <div> Hero </div>;
+  return (
+    <>
+      <section>
+        <div className="row">
+          <div className="col-5">
+            <p className="h-par">
+              <span>
+                <img src={discount} alt="discount" className="dis-img" />
+              </span>
+              <span> 20%</span> Discount for <span>1 month</span> Account
+            </p>
+            <h1>
+              The Next <br />
+              <span id="h1-col">Generation</span>
+              <br />
+              Payment Method.
+            </h1>
+            <p className="par-her0">
+              Our team of experts uses a methodology to identify the credit
+              cards most likely to fit your needs.<br/> We examine annual percentage
+              rates, annual fees.
+            </p>
+           
+          </div>
+       <div className="col-1 btnh"> <GetStarted /> </div>
+          <div className="col-5 menu">
+            <img src={robot} alt="billing" className="img-robot" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 export default Hero;
