c226a8c64f10f0a61ee5beb78b56bb5d48292cc2
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-09-06 17:16:49 +0100

    Update business

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,30 +1,43 @@
 import { features } from "../constatnts/Data";
 
+
+
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div>
+    <div className="business-t">
+      <img src={icon} alt="star" className="star-img" />
+      <h4 className="b-title">{title}</h4>
+      <p className="b-content">{content}</p>
+    </div>
+  </div>
+);
 const Business = () => {
   return (
     <section>
       <div className="business">
         <div className="row">
-          <div className="col-6">
-            <h1>The City</h1>
-            <p>
-              Chania is the capital of the Chania region on the island of Crete.
-              The city can be divided in two parts, the old town and the modern
-              city.
+          <div className="col-6 b-text">
+            <h2 className="h1-b">
+              You do the business, <br /> we’ll handle the money.
+            </h2>
+            <p className="p-b">
+              With the right credit card, you can improve your financial life by
+              building credit, <br />
+              earning rewards and saving money. <br />
+              But with hundreds of credit cards on the market.
             </p>
           </div>
 
           <div className="col-5 ">
-            <ul>
-              <li>The Flight</li>
-              <li>The City</li>
-              <li>The Island</li>
-              <li>The Food</li>
-            </ul>
+            <div className="">
+              {features.map((feature, index) => (
+                <FeatureCard key={feature.id} {...feature} index={index} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-      Business{" "}
     </section>
   );
 };
