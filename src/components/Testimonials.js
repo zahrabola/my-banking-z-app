371653a397bff6e8cad636a0f196df5221ab0ce2
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-09-07 17:16:24 +0100

    Update testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,18 @@
 const Testimonials = () => {
-  return <div> Testimonials </div>;
+  return (
+    <section>
+      <div className="row">
+        <div className="col-6">
+          <h2 className="h1-b">
+            {" "}
+            What People are <br /> saying about us
+          </h2>
+          
+        </div>
+      </div>
+      <div className="row">feedback</div>
+      <div> Testimonials </div>
+    </section>
+  );
 };
 export default Testimonials;
