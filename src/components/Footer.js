088bac0caf5260a8e5a51de78ccff6e39a06be7d
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-09-09 23:59:11 +0100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,39 @@
+import {  logo} from "../assets";
+import { footerLinks} from "../constatnts/Data";
+
 const Footer = () => {
-  return <div> Footer</div>;
+  return (
+    <section>
+      <div>
+        <div className="row">
+          <div className="col-6">
+            <img src={logo} alt="z-bank" className="z-bank" />
+            <p className="">
+              A new way to make the payments easy, reliable and secure.
+            </p>
+          </div>
+          <div className="col-6">
+            {footerLinks.map((footerlink) => (
+              <div key={footerlink.title} className="footer-link">
+                <h4>{footerlink.title}</h4>
+                <ul className="list-none mt-4">
+                  {footerlink.links.map((link, index) => (
+                    <li
+                      key={link.name}
+                      className={` ${
+                        index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                      }`}
+                    >
+                      {link.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Footer;
